2411da29f49d314714f838c1e2d25b878216d917
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-03-26 14:43:37 -0400

    npm audit fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,45 @@
 "use client";
 import React from "react";
 import HeroParallaxDemo from "~/components/example/hero-parallax-demo";
-import TextHoverEffectDemo from "~/components/example/text-hover-effect-demo";
 import NavbarDemo from "~/components/example/navbar-menu-demo";
 import GoogleGeminiEffectDemo from "~/components/example/google-gemini-effect-demo";
+import { HeroHighlight, Highlight } from "~/components/ui/hero-highlight";
+import { motion } from "framer-motion";
+import { GlowingEffectDemo } from "~/components/example/glowing-effect-demo";
 
 export default function Page() {
   return (
     <div className="bg-black">
       <NavbarDemo />
-      <div>
-        <TextHoverEffectDemo />
+      <div className="flex flex-col md:flex-row items-center justify-between w-full px-8 py-24">
+        {/* Left Side: Title */}
+        <div className="w-full md:w-1/2 flex justify-center">
+          <HeroHighlight>
+            <motion.h1
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: [20, -5, 0] }}
+              transition={{ duration: 0.5, ease: [0.4, 0.0, 0.2, 1] }}
+              className="text-white px-4 md:text-4xl lg:text-5xl font-bold leading-relaxed lg:leading-snug text-center md:text-left"
+            >
+              <div className="text-6xl mb-8">Compete with</div>
+              <Highlight className="pb-2 text-black text-8xl dark:text-white">
+                Swiftplay
+              </Highlight>
+            </motion.h1>
+          </HeroHighlight>
+        </div>
+
+        {/* Right Side: Glowing Effect Demo */}
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: [20, -5, 0] }}
+          transition={{ duration: 1, ease: [0.4, 0.0, 0.2, 1] }}
+          className="w-full md:w-1/2 flex justify-center"
+        >
+          <GlowingEffectDemo />
+        </motion.div>
       </div>
+
       <div>
         <GoogleGeminiEffectDemo />
       </div>
