1077a7716513afec9e7691c08213471a68369055
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-03-27 01:19:31 -0400

    New header

--- src/components/example/glowing-effect-demo.tsx
+++ src/components/example/glowing-effect-demo.tsx
@@ -8,37 +8,37 @@ export function GlowingEffectDemo() {
     <ul className="grid grid-cols-1 grid-rows-none gap-4 md:grid-cols-12 md:grid-rows-3 lg:gap-4 xl:max-h-[34rem] xl:grid-rows-2">
       <GridItem
         area="md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]"
-        icon={<Box className="h-4 w-4 text-black dark:text-neutral-400" />}
-        title="Do things the right way"
-        description="Running out of copy so I'll write anything."
+        icon={<Box className="h-4 w-4 text-white dark:text-neutral-400" />}
+        title="Compete Globally"
+        description="Speedrun against the best around the globe."
       />
 
       <GridItem
         area="md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]"
-        icon={<Settings className="h-4 w-4 text-black dark:text-neutral-400" />}
-        title="The best AI code editor ever."
-        description="Yes, it's true. I'm not even kidding. Ask my mom if you don't believe me."
+        icon={<Settings className="h-4 w-4 text-white dark:text-neutral-400" />}
+        title="Rank up and earn."
+        description="Unlock Customizations as you climb the leaderboard."
       />
 
       <GridItem
         area="md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]"
-        icon={<Lock className="h-4 w-4 text-black dark:text-neutral-400" />}
-        title="You should buy Aceternity UI Pro"
-        description="It's the best money you'll ever spend"
+        icon={<Lock className="h-4 w-4 text-white dark:text-neutral-400" />}
+        title="Unlock Tutorials, Themes, and more."
+        description="With Swiftplay Pro, you can unlock a world of possibilities."
       />
 
       <GridItem
         area="md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]"
-        icon={<Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />}
-        title="This card is also built by Cursor"
-        description="I'm not even kidding. Ask my mom if you don't believe me."
+        icon={<Sparkles className="h-4 w-4 text-white dark:text-neutral-400" />}
+        title="Stay in touch."
+        description="With our AI powered notifications, you'll never miss a new trend."
       />
 
       <GridItem
         area="md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]"
-        icon={<Search className="h-4 w-4 text-black dark:text-neutral-400" />}
-        title="Coming soon on Aceternity UI"
-        description="I'm writing the code as I record this, no shit."
+        icon={<Search className="h-4 w-4 text-white dark:text-neutral-400" />}
+        title="Now on Mobile!"
+        description="Download Swiftplay on iOS or Android!"
       />
     </ul>
   );
@@ -62,7 +62,7 @@ const GridItem = ({ area, icon, title, description }: GridItemProps) => {
           proximity={64}
           inactiveZone={0.01}
         />
-        <div className="relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl border-0.75 p-6  dark:shadow-[0px_0px_27px_0px_#2D2D2D] md:p-6">
+        <div className="relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl border-0.75 p-6  shadow-[0px_0px_27px_0px_#2D2D2D] md:p-6">
           <div className="relative flex flex-1 flex-col justify-between gap-3">
             <div className="w-fit rounded-lg border border-gray-600 p-2 ">
               {icon}
