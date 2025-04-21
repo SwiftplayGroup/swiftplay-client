ec48c16d33de08b2e45c5820f9e5c6159c892d34
Christian Toney <toney1c@cmich.edu>
2025-04-20 23:22:11 -0400

    Add feature: Game pages • Group runs by category (#151)
    * add run category
    
    * Add feature: Game pages • Group runs by category

--- src/app/games/[gameID]/components/GameRunsCard/GameRunsCard.tsx
+++ src/app/games/[gameID]/components/GameRunsCard/GameRunsCard.tsx
@@ -9,11 +9,14 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~
 import Link from "next/link";
 import convertMillisecondsToTime from "~/lib/millisecondsToTime";
 import Game from "~/api/Game";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
+import Category from "~/api/Category";
 
 export default function GameRunsCard({game}: {game: Game}) {
 
   const [isLoading, setIsLoading] = useState<boolean>(!!game);
   const [runs, setRuns] = useState<Run[]>([]);
+  const [selectedCategoryID, setSelectedCategoryID] = useState<string>("default");
 
   useEffect(() => {
 
@@ -60,14 +63,44 @@ export default function GameRunsCard({game}: {game: Game}) {
 
   }, [game]);
 
+  const categoryOptions: Category[] = [];
+  for (const run of runs) {
+
+    if (run.category && !categoryOptions.find((category) => category._id === run.category?._id)) {
+
+      categoryOptions.push(run.category);
+
+    }
+
+  }
+
+  const filteredRuns = runs.filter((run) => (!run.category && selectedCategoryID === "default") || (run.category?._id === selectedCategoryID));
+
   return (
     <Card className={styles.container}>
       <CardTitle>Runs</CardTitle>
+      <section>
+        <Select value={selectedCategoryID} disabled={categoryOptions.length < 1} onValueChange={(newSelectedCategoryID) => setSelectedCategoryID(newSelectedCategoryID)}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="default">Default</SelectItem>
+            {
+              categoryOptions.map((category) => (
+                <SelectItem key={category._id} value={category._id}>
+                  {category.name}
+                </SelectItem>
+              ))
+            }
+          </SelectContent>
+        </Select>
+      </section>
       {
-        isLoading || !game ? (
+        isLoading ? (
           <Skeleton className="h-4 w-[250px]" />
         ) : (
-          runs[0] ? (
+          filteredRuns[0] ? (
             <Table>
               <TableHeader>
                 <TableRow>
@@ -78,7 +111,7 @@ export default function GameRunsCard({game}: {game: Game}) {
               </TableHeader>
               <TableBody>
                 {
-                  runs.map((run, index) => (
+                  filteredRuns.map((run, index) => (
                     <TableRow key={run._id}>
                       <TableCell>
                         {index + 1}
