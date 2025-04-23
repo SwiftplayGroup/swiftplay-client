436e1b6aedd08f93608e919d5cc68d95ce54f10f
Michael Strange <66543114+michael-strange@users.noreply.github.com>
2025-04-23 13:39:01 -0400

    Polishing format and fix scrollbar (#175)
    * Fixed scrolling on about us page
    
    * Fixed punctuation

--- src/components/about/flip-words.tsx
+++ src/components/about/flip-words.tsx
@@ -6,8 +6,8 @@ export function FlipWordsSection() {
   const words = ["modern", "functional", "stunning", "unique"];
 
   return (
-    <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="text-6xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+    <div className="flex justify-center items-center py-20 px-4">
+      <div className="text-4xl md:text-6xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         Build
         <FlipWords words={words} /> <br />
         websites with the devs at{" "}
